a78a3c937527975737203818bcb8345376af00ca
Aneta Potomská <potomane@fit.cvut.cz>
2024-03-14 02:41:28 +0100

    added more tools, better formating

--- web/lib/data/enumMapping/jobTypeMapping.ts
+++ web/lib/data/enumMapping/jobTypeMapping.ts
@@ -2,9 +2,9 @@ import { JobType } from '../../prisma/client'
 import { EnumMapping } from './enumMapping'
 
 export const jobTypeMapping: EnumMapping<keyof typeof JobType> = {
-  WOOD: 'Dřevo',
-  PAINTING: 'Malování',
-  HOUSEWORK: 'Pomoc doma',
-  GARDEN: 'Práce na zahradě',
-  OTHER: 'Ostatní',
+  WOOD      : 'Dřevo',
+  PAINTING  : 'Malování',
+  HOUSEWORK : 'Pomoc doma',
+  GARDEN    : 'Práce na zahradě',
+  OTHER     : 'Ostatní',
 }
