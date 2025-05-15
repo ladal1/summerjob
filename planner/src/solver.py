88bd455d6ac5f3564648076330fa0d0ccda2f04d
illia-ua <illia.udovichenko27@gmail.com>
2025-05-15 15:40:23 +0200

    Adjusted solver.py

--- planner/src/solver.py
+++ planner/src/solver.py
@@ -4,45 +4,33 @@ from psycopg2 import extras
 from pulp import LpMinimize, LpProblem, lpSum, LpVariable
 import pandas as pd
 import uuid
-
 from dotenv import load_dotenv
+
+from planner.src.queries import *
+
 # Load variables from .env
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-from planner.src.queries import *
-
 
 def dictionarify(query_results):
-    dictionary = {}
-    for row in query_results:
-        dictionary[row["id"]] = {**row}
-    return dictionary
+    return {row["id"]: {**row} for row in query_results}
 
 
 def transform_score(query_results):
-    dictionary = {}
-    for row in query_results:
-        dictionary[(row["job"], row["worker"])] = row["score"]
-    return dictionary
+    return {(row["job"], row["worker"]): row["score"] for row in query_results}
 
 
 def is_viable(worker, job, forbidden, attempt):
-    # allergens & adoration
-    forbid = forbidden[worker["id"]] if worker["id"] in forbidden.keys() else []
     if not (set(worker["allergies"]).isdisjoint(job["allergens"]) and
-            ((worker["isAdoring"] and job["supportsAdoration"]) or not
-            worker["isAdoring"]) and
+            ((worker["isAdoring"] and job["supportsAdoration"]) or not worker["isAdoring"]) and
             (job not in forbidden or attempt > 1)):
         return True
     return False
 
 
 def what_workers(row, plan):
-    workers = []
-    for index, value in row.items():
-        if value.varValue > 0:
-            workers.append(index)
+    workers = [index for index, value in row.items() if value.varValue > 0]
     plan[row.name] = workers
 
 
@@ -56,9 +44,8 @@ def generate_plan(plan_id, connection, first_round=True, attempt=0):
     dict_cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     primitive_cursor = connection.cursor()
 
-    # Fetch all the records
     primitive_cursor.execute(select_jobs, {"planId": plan_id})
-    jobs = list(map(lambda x: x[0], primitive_cursor.fetchall()))
+    jobs = [x[0] for x in primitive_cursor.fetchall()]
 
     job_properties = load(dict_cursor, plan_id, select_job_details)
     workers = load(dict_cursor, plan_id, select_strong_workers if first_round else select_workers)
@@ -66,71 +53,61 @@ def generate_plan(plan_id, connection, first_round=True, attempt=0):
     forbidden_jobs = load(dict_cursor, plan_id, select_forbidden_jobs)
     active_jobs = load(dict_cursor, plan_id, select_active_jobs)
     areas = load(dict_cursor, plan_id, select_areas)
+
     dict_cursor.execute(select_score, {"planId": plan_id})
     scores = transform_score(dict_cursor.fetchall())
 
     model = LpProblem(name="Plan", sense=LpMinimize)
     model_variables = pd.DataFrame(columns=workers.keys())
-
     score = []
-
     counter = 0
-    area_drivers = dict.fromkeys(areas.keys(), [])
+    area_drivers = {area: [] for area in areas}
 
     for job in jobs:
-        job_vars = {}
-        strongman = []
-        driver = []
+        job_vars, strongman, driver = {}, [], []
         for worker in workers:
             if is_viable(workers[worker], job_properties[job], forbidden_jobs, attempt):
-                add_variable(counter,
-                             driver,
-                             first_round,
-                             job_properties[job],
-                             job_vars,
-                             strongman,
-                             worker,
-                             workers,
-                             area_drivers,
-                             score, scores)
+                add_variable(counter, driver, first_round, job_properties[job], job_vars,
+                             strongman, worker, workers, area_drivers, score, scores)
                 counter += 1
 
         df_new_row = pd.DataFrame([pd.Series(job_vars, name=job)], columns=model_variables.columns)
         model_variables = pd.concat([model_variables, df_new_row])
+
         max_workers = job_properties[job]["maxWorkers"]
         min_workers = job_properties[job]["minWorkers"]
 
-        model += (lpSum(job_vars.values()) <= max_workers)
+        model += lpSum(job_vars.values()) <= max_workers
         if first_round:
-            model += (lpSum(strongman) >= job_properties[job]["strongWorkers"])
+            model += lpSum(strongman) >= job_properties[job]["strongWorkers"]
             if attempt < 1:
-                model += (lpSum(driver) >= job_properties[job]["neededCars"])
+                model += lpSum(driver) >= job_properties[job]["neededCars"]
         else:
-            model += (lpSum(job_vars.values()) >= min_workers)
+            model += lpSum(job_vars.values()) >= min_workers
+
     for worker in workers:
-        model += (lpSum(model_variables[worker].tolist()) <= 1)
-        model += (lpSum(model_variables[worker].tolist()) >= 1)
+        model += lpSum(model_variables[worker].tolist()) == 1
 
     if attempt < 2:
         for forbid in forbids:
             friend = forbids[forbid]["forbid"]
-            if friend not in workers.keys() or forbid not in workers.keys():
-                continue
-            for job in jobs:
-                model = restrict_pair(forbid, friend, job, model, model_variables)
+            if friend in workers and forbid in workers:
+                for job in jobs:
+                    model = restrict_pair(forbid, friend, job, model, model_variables)
+
     if attempt > 0:
         for area in areas:
-            model += (lpSum(area_drivers[area]) >= areas[area]["requiredDrivers"])
+            model += lpSum(area_drivers[area]) >= areas[area]["requiredDrivers"]
 
     model += lpSum(score)
 
     status = model.solve()
     if status == -1:
         if attempt >= 2:
-            print("fail")  # ToDo - send message: check if valid - enough drivers, strongmen, workers
+            print("fail")
             return
         else:
-            generate_plan(test_plan_id, test_connection, first_round, attempt + 1)
+            generate_plan(plan_id, connection, first_round, attempt + 1)
             return
 
     res_dict = {}
@@ -142,21 +119,20 @@ def generate_plan(plan_id, connection, first_round=True, attempt=0):
 
 def load(dict_cursor, plan_id, query):
     dict_cursor.execute(query, {"planId": plan_id})
-    active_jobs = dictionarify(dict_cursor.fetchall())
-    return active_jobs
+    return dictionarify(dict_cursor.fetchall())
 
 
 def restrict_pair(forbid, friend, job, model, model_variables):
     if model_variables.at[job, forbid] is not None and model_variables.at[job, friend] is not None:
-        model += (model_variables.at[job, forbid] + model_variables.at[job, friend]) <= 1
+        model += model_variables.at[job, forbid] + model_variables.at[job, friend] <= 1
     return model
 
 
 def add_variable(counter, driver, first_round, job, job_vars, strongman, worker, workers, area_driver, score, scores):
-    name = "x" + str(counter)
+    name = f"x{counter}"
     x = LpVariable(name, lowBound=0, upBound=1, cat='Binary')
     job_vars[worker] = x
-    if (job["id"], worker) in scores.keys():
+    if (job["id"], worker) in scores:
         score.append(scores[(job["id"], worker)] * x)
     if first_round:
         if workers[worker]["isStrong"]:
@@ -168,7 +144,6 @@ def add_variable(counter, driver, first_round, job, job_vars, strongman, worker,
 
 def generate_rides(received_plan_id, connection):
     dict_cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    primitive_cursor = connection.cursor()
     psycopg2.extras.register_uuid()
 
     jobs = load(dict_cursor, received_plan_id, select_drive_jobs)
@@ -177,7 +152,7 @@ def generate_rides(received_plan_id, connection):
         people = list(load(dict_cursor, job, select_people).keys()) + list(drivers.keys())
         people_pointer = 0
         for driver in drivers:
-            if len(people) == 0:
+            if not people:
                 break
             people.remove(driver)
             ride = uuid.uuid4()
@@ -191,19 +166,9 @@ def generate_rides(received_plan_id, connection):
                 people_pointer += 1
                 seats -= 1
 
-    # find_people without ride people for area
-    # find empty rides - id = area, ride, seats ORDER by seats
-
-    # for people without ride
-    # find best empty ride in the same area
-    # add to ride(ride, worker)
-
 
 def generate_plan_from_message(received_plan_id):
-    connection = psycopg2.connect(DATABASE_URL,
-                                  options="-c search_path=public")
+    connection = psycopg2.connect(DATABASE_URL, options="-c search_path=public")
     generate_rides(received_plan_id, connection)
-
     # generate_plan(received_plan_id, connection)
     # generate_plan(received_plan_id, connection, False)
-    # generate_rides(received_plan_id, connection)
