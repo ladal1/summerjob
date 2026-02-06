422062cbae9489d8bf3f8e1d90e810d17310f846
David <krejcd10@fit.cvut.cz>
2026-02-06 08:37:08 +0100

    Fix eslint config

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,22 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-    rules: {
-      "@typescript-eslint/no-unused-expressions": "off",
-    },
-    ignorePatterns: ["lib/prisma/**/*", "node_modules/**/*", "build/**/*"],
-  }),
-];
-
-export default eslintConfig;
+// https://nextjs.org/docs/app/api-reference/config/eslint
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
+ 
+export default eslintConfig
